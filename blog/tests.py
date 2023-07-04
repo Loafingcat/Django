b0dc0f504c97a2e532f2cabd1eccbd22657b5ca5
Junho Byun <bjunho0130@gmail.com>
2023-07-05 02:07:44 +0900

    13장 진행중

--- blog/tests.py
+++ blog/tests.py
@@ -1,11 +1,47 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
-from .models import Post
+from django.contrib.auth.models import User
+from .models import Post, Category
 
 
 class TestView(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_jh = User.objects.create_user(username='jh', password='loveyou0130')
+        self.user_ssilssel = User.objects.create_user(username='ssilssel', password='loveyou0130')
+
+        self.category_programming = Category.objects.create(name='programming', slug='programming')
+        self.category_music = Category.objects.create(name='music', slug='music')
+
+        self.post_001 = Post.objects.create(
+            title='첫 번째 포스트입니다.',
+            content='Hello World. We are the world.',
+            category=self.category_programming,
+            author=self.user_jh
+        )
+
+        self.post_002 = Post.objects.create(
+            title='두 번째 포스트입니다.',
+            content='1등이 전부는 아니잖아요.',
+            category=self.category_programming,
+            author=self.user_ssilssel
+        )
+
+    def test_category_page(self):
+        response = self.client.get(self.category_programming.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.navbar_test(soup)
+        self.category_card_test(soup)
+
+        self.assertIn(self.category_programming.name, soup.h1.text)
+
+        main_area = soup.find('div', id='main-area')
+        self.assertIn(self.category_programming.name, main_area.text)
+        self.assertIn(self.post_001.title, main_area.text)
+        self.assertNotIn(self.post_002.title, main_area.text)
+        self.assertNotIn(self.post_003.title, main_area.text)
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -24,59 +60,67 @@ class TestView(TestCase):
         about_me_btn = navbar.find('a', text='About Me')
         self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
 
+    def category_card_test(self, soup):
+        categories_card = soup.find('div', id='categories-card')
+        self.assertIn('Categories', categories_card.text)
+        self.assertIn(f'{self.category_programming.name} ({self.category_programming.post_set.count()})',
+                      categories_card.text)
+        self.assertIn(f'{self.category_music.name} ({self.category_music.post_set.count()})',
+                      categories_card.text)
+        self.assertIn(f'미분류 (0)', categories_card.text)
+
     def test_post_list(self):
-        response = self.client.get('/blog/')
-
-        self.assertEqual(response.status_code, 200)
-
-        soup = BeautifulSoup(response.content, 'html.parser')
-        self.assertEqual(soup.title.text, 'Blog')
-
-        self.navbar_test(soup)
-
-        self.assertEqual(Post.objects.count(), 0)
-
-        main_area = soup.find('div', id='main-area')
-        self.assertIn('아직 게시물이 없습니다', main_area.text)
-
-        post_001 = Post.objects.create(
-            title='첫 번째 포스트입니다.',
-            content='Hello World. We are the world',
-        )
-        post_002 = Post.objects.create(
-            title='두 번째 포스트입니다.',
-            content='1등이 전부는 아니잖아요?',
-        )
         self.assertEqual(Post.objects.count(), 2)
 
         response = self.client.get('/blog/')
-        soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.navbar_test(soup)
+        self.category_card_test(soup)
 
         main_area = soup.find('div', id='main-area')
-        self.assertIn(post_001.title, main_area.text)
-        self.assertIn(post_002.title, main_area.text)
-
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
 
+        post_001_card = main_area.find('div', id='post-1')
+        self.assertIn(self.post_001.title, post_001_card.text)
+        self.assertIn(self.post_001.category.name, post_001_card.text)
+
+        post_002_card = main_area.find('div', id='post-2')
+        self.assertIn(self.post_002.title, post_002_card.text)
+        self.assertIn(self.post_002.category.name, post_002_card.text)
+
+        self.assertIn(self.user_jh.username.upper(), main_area.text)
+        self.assertIn(self.user_ssilssel.username.upper(), main_area.text)
+
+        Post.objects.all().delete()
+        self.assertEqual(Post.objects.count(), 0)
+        response = self.client.get('/blog/')
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_area = soup.find('div', id='main-area')
+        self.assertIn('아직 게시물이 없습니다', main_area.text)
+
     def test_post_detail(self):
-        post_001 = Post.objects.create(
+        post_000 = Post.objects.create(
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world.',
+            author=self.user_jh,
         )
 
-        self.assertEqual(post_001.get_absolute_url(), '/blog/1/')
+        self.assertEqual(post_000.get_absolute_url(), '/blog/3/')
 
-        response = self.client.get(post_001.get_absolute_url())
+        response = self.client.get(post_000.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         self.navbar_test(soup)
 
-        self.assertIn(post_001.title, soup.title.text)
+        self.assertIn(post_000.title, soup.title.text)
 
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001.title, post_area.text)
+        self.assertIn(post_000.title, post_area.text)
 
-        self.assertIn(post_001.content, post_area.text)
+        self.assertIn(self.user_jh.username.upper(), post_area.text)
+
+        self.assertIn(post_000.content, post_area.text)
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+
+        return context
 
 
 class PostDetail(DetailView):
@@ -32,5 +39,25 @@ def single_post_page(request, pk):
         'blog/post_detail.html',
         {
             'post': post,
+        }
+    )
+
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
         }
     )
