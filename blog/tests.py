16a14622e87997139fb4486f190b3d15fc7ed1e0
Junho Byun <bjunho0130@gmail.com>
2023-07-07 04:14:22 +0900

    14장 진행중 - 포스트 목록 페이지와 포스트 상세 페이지에 태그 기능 추가

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 
 class TestView(TestCase):
@@ -13,12 +13,17 @@ class TestView(TestCase):
         self.category_programming = Category.objects.create(name='programming', slug='programming')
         self.category_music = Category.objects.create(name='music', slug='music')
 
+        self.tag_python_kor = Tag.objects.create(name='파이썬 공부', slug='파이썬-공부')
+        self.tag_python = Tag.objects.create(name='python', slug='python')
+        self.tag_hello = Tag.objects.create(name='hello', slug='hello')
+
         self.post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world.',
             category=self.category_programming,
             author=self.user_jh
         )
+        self.post_001.tags.add(self.tag_hello)
 
         self.post_002 = Post.objects.create(
             title='두 번째 포스트입니다.',
@@ -26,6 +31,8 @@ class TestView(TestCase):
             category=self.category_programming,
             author=self.user_ssilssel
         )
+        self.post_002.tags.add(self.tag_python_kor)
+        self.post_002.tags.add(self.tag_python)
 
     def test_category_page(self):
         response = self.client.get(self.category_programming.get_absolute_url())
@@ -83,10 +90,16 @@ class TestView(TestCase):
         post_001_card = main_area.find('div', id='post-1')
         self.assertIn(self.post_001.title, post_001_card.text)
         self.assertIn(self.post_001.category.name, post_001_card.text)
+        self.assertIn(self.tag_hello.name, post_001_card.text)
+        self.assertNotIn(self.tag_python.name, post_001_card.text)
+        self.assertNotIn(self.tag_python_kor.name, post_001_card.text)
 
         post_002_card = main_area.find('div', id='post-2')
         self.assertIn(self.post_002.title, post_002_card.text)
         self.assertIn(self.post_002.category.name, post_002_card.text)
+        self.assertNotIn(self.tag_hello.name, post_002_card.text)
+        self.assertIn(self.tag_python.name, post_002_card.text)
+        self.assertIn(self.tag_python_kor.name, post_002_card.text)
 
         self.assertIn(self.user_jh.username.upper(), main_area.text)
         self.assertIn(self.user_ssilssel.username.upper(), main_area.text)
@@ -99,26 +112,30 @@ class TestView(TestCase):
         self.assertIn('아직 게시물이 없습니다', main_area.text)
 
     def test_post_detail(self):
-        post_000 = Post.objects.create(
+        post_001 = Post.objects.create(
             title='첫 번째 포스트입니다.',
             content='Hello World. We are the world.',
             author=self.user_jh,
         )
 
-        self.assertEqual(post_000.get_absolute_url(), '/blog/3/')
+        self.assertEqual(post_001.get_absolute_url(), '/blog/3/')
 
-        response = self.client.get(post_000.get_absolute_url())
+        response = self.client.get(post_001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         self.navbar_test(soup)
 
-        self.assertIn(post_000.title, soup.title.text)
+        self.assertIn(post_001.title, soup.title.text)
 
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_000.title, post_area.text)
+        self.assertIn(post_001.title, post_area.text)
 
         self.assertIn(self.user_jh.username.upper(), post_area.text)
 
-        self.assertIn(post_000.content, post_area.text)
+        self.assertIn(post_001.content, post_area.text)
+
+        self.assertIn(self.tag_hello.name, post_area.text)
+        self.assertNotIn(self.tag_python.name, post_area.text)
+        self.assertNotIn(self.tag_python_kor.name, post_area.text)
