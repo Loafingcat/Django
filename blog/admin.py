18e9096bcb92aa2e6d8696e2f0394a68a4f8a5b1
Junho Byun <bjunho0130@gmail.com>
2023-07-25 22:48:06 +0900

    17장 진행중 - 댓글을 포스트 상세 페이지에 띄우기

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Comment)
 
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
+
 
 class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
@@ -14,3 +16,4 @@ class TagAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
+
--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 
 class TestView(TestCase):
@@ -35,6 +35,12 @@ class TestView(TestCase):
         )
         self.post_002.tags.add(self.tag_python_kor)
         self.post_002.tags.add(self.tag_python)
+
+        self.comment_001 = Comment.objects.create(
+            post=self.post_001,
+            author=self.user_jh,
+            content='첫 번째 댓글입니다.'
+        )
 
     def test_tag_page(self):
         response = self.client.get(self.tag_hello.get_absolute_url())
@@ -156,6 +162,11 @@ class TestView(TestCase):
         self.assertIn(self.tag_hello.name, post_area.text)
         self.assertNotIn(self.tag_python.name, post_area.text)
         self.assertNotIn(self.tag_python_kor.name, post_area.text)
+
+        commenta_area = soup.find('div', id='comment-area')
+        comment_001_area = commenta_area.find('div', id='comment-1')
+        self.assertIn(self.comment_001.author.username, comment_001_area.text)
+        self.assertIn(self.comment_001, comment_001_area.text)
 
     def test_create_post(self):
 
